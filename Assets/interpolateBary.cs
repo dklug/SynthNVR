abc3418d3e0d4a32d3453d8a09cadade08424240
Duncan <dklug@ucmerced.edu>
2018-05-09 13:34:13 -0700

    Added seperate '2d' representations of tetrahedral

--- Assets/interpolateBary.cs
+++ Assets/interpolateBary.cs
@@ -4,68 +4,147 @@ using UnityEngine;
 using System.Net.Sockets;
 using VRTK;
 
-public class interpolateBary : VRTK_InteractableObject{
+public class interpolateBary : VRTK_InteractableObject
+{
 
-    public List<Transform> tetraTransforms;
+    public Transform[] tetraTransforms;
+    public Transform[] sliderBasePoints;
+    public GameObject[] sliders;
+    Vector3 lastPosition;
+
     public int soundServerPort = 4242;
     public string soundServerAddr = "127.0.0.1";
     private TcpClient serverConn;
 
-    float p1, p2, p3, p4;
+    public float[] points = new float[4];
 
-    void updateBarycentric() {
-        float p1Dist = Vector3.Distance(tetraTransforms[0].position, transform.position);
-        float p2Dist = Vector3.Distance(tetraTransforms[1].position, transform.position);
-        float p3Dist = Vector3.Distance(tetraTransforms[2].position, transform.position);
-        float p4Dist = Vector3.Distance(tetraTransforms[3].position, transform.position);
 
-        float normalizer = Mathf.Sqrt(Mathf.Pow(p1Dist, 2) + Mathf.Pow(p2Dist, 2) + Mathf.Pow(p3Dist, 2) + Mathf.Pow(p4Dist, 2));
+    float[] normalize(float[] distances)
+    {
+        float sumSquares = 0;
+        for (int i = 0; i < distances.Length; ++i)
+        {
+            sumSquares += Mathf.Pow(distances[i], 2);
+        }
+        float normalizer = Mathf.Sqrt(sumSquares);
+        // Handle divide by zero
+        if (normalizer == 0)
+        {
+            distances[0] = 1;
+            return distances;
+        }
 
-        p1 = p1Dist / normalizer;
-        p2 = p2Dist / normalizer;
-        p3 = p3Dist / normalizer;
-        p4 = p4Dist / normalizer;
+        for (int i = 0; i < distances.Length; ++i)
+        {
+            distances[i] /= normalizer;
+        }
+        return distances;
     }
 
-    void transmitCoordinates() {
-        if(serverConn == null) {
+    void updateBarycentric()
+    {
+        float[] dist = new float[4];
+        float sumSquares = 0;
+        for (int i = 0; i < tetraTransforms.Length; ++i)
+        {
+            dist[i] = 0.7f - Vector3.Distance(tetraTransforms[i].position, transform.position);
+            //sumSquares += Mathf.Pow (dist [i], 2);
+        }
+        /*
+		float normalizer = Mathf.Sqrt(sumSquares);
+		for (int i = 0; i < tetraTransforms.Length; ++i) {
+			points [i] = dist [i] / normalizer;
+		}
+		*/
+        points = normalize(dist);
+    }
+
+    void updateSliders()
+    {
+        for (int i = 0; i < sliderBasePoints.Length; ++i)
+        {
+            Vector3 newPosition = new Vector3(sliderBasePoints[i].position.x, sliderBasePoints[i].position.y, sliderBasePoints[i].position.z);
+            newPosition.y += points[i];
+            sliders[i].transform.position = newPosition;
+        }
+    }
+
+    public void updateControlFromSliders()
+    {
+        Debug.Log("Updating from sliders");
+        float[] distances = new float[4];
+        for (int i = 0; i < sliders.Length; ++i)
+        {
+            distances[i] = Vector3.Distance(sliderBasePoints[i].position, sliders[i].transform.position);
+        }
+        distances = normalize(distances);
+
+        // Convert barycentric coords to xyz for
+        Vector3 newControlPosition;
+        newControlPosition.x = newControlPosition.y = newControlPosition.z = 0;
+        for (int i = 0; i < tetraTransforms.Length; ++i)
+        {
+            newControlPosition.x += distances[i] * tetraTransforms[i].position.x;
+            newControlPosition.y += distances[i] * tetraTransforms[i].position.y;
+            newControlPosition.z += distances[i] * tetraTransforms[i].position.z;
+        }
+    }
+
+    void transmitCoordinates()
+    {
+        if (serverConn == null)
+        {
             Debug.Log("Server connection isnt open for transmitting coordinates");
             return;
         }
-        try {
+        try
+        {
             NetworkStream stream = serverConn.GetStream();
-            if(stream.CanWrite) {
+            if (stream.CanWrite)
+            {
                 byte[] coords = new byte[4];
-                coords[0] = (byte)Mathf.Round(p1 * 10);
-                coords[1] = (byte)Mathf.Round(p2 * 10);
-                coords[2] = (byte)Mathf.Round(p3 * 10);
-                coords[3] = (byte)Mathf.Round(p4 * 10);
+                for (int i = 0; i < points.Length; ++i)
+                {
+                    coords[i] = (byte)Mathf.Round(points[i] * 10);
+                }
                 stream.Write(coords, 0, 4);
-                Debug.Log("Just sent coords to server");
-            } else {
+                //Debug.Log("Just sent coords to server");
+            }
+            else
+            {
                 Debug.Log("Socket isnt writeable");
             }
 
-        } catch (SocketException sockExcep)
+        }
+        catch (SocketException sockExcep)
         {
             Debug.Log("Socket exception " + sockExcep);
         }
     }
 
-	// Use this for initialization
-	void Start () {
-        try {
+    // Use this for initialization
+    void Start()
+    {
+        try
+        {
             serverConn = new TcpClient(soundServerAddr, soundServerPort);
-        } catch (SocketException sockExcep) {
+        }
+        catch (SocketException sockExcep)
+        {
             Debug.Log("Socket exception " + sockExcep);
         }
     }
-	
-	// Update is called once per frame
-	void Update () {
-        if (IsGrabbed()) {
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lastPosition == null || transform.position != lastPosition)
+        {
             updateBarycentric();
             transmitCoordinates();
+            updateSliders();
+            lastPosition = transform.position;
         }
-	}
+    }
 }
+
---
+++ Assets/sliderCtl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class sliderCtl : VRTK_InteractableObject {
+
+	public Transform sliderBase;
+	public interpolateBary controlSphere;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (IsGrabbed ()) {
+            Vector3 clampedPos = transform.position;
+            clampedPos.x = sliderBase.position.x;
+            clampedPos.z = sliderBase.position.z;
+            transform.position = clampedPos;
+			controlSphere.updateControlFromSliders ();
+		}
+	}
+}
